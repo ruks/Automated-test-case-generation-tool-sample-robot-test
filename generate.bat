35931d3fde1dce8b058d481b9f961b33f2c6b39c
ruks <ruky68@live.com>
2014-08-17 21:33:06 +0530

    add generate.bat

---
+++ generate.bat
@@ -0,0 +1,30 @@
+@echo off
+
+IF "%1" == "-help" goto showHelp
+
+IF "%1" =="" (
+	goto showError
+)else ( goto generate)
+
+
+:showHelp
+echo ------Automated test case genagation tool------
+echo run sh generate.sh [package name]
+echo [package name] is package name libraries should be generated!
+echo   
+goto end
+
+:showError
+echo ------ ------ error ------ ------
+echo Invalid argumnet!
+echo run sh generate.sh -help for help
+echo   
+goto showHelp
+
+:generate
+echo ------------Generating libraries------------
+mvn exec:java -Dexec.mainClass="generator.ClientGenerator" -Dexec.args="%1"
+goto end
+
+
+:end
